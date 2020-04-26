e55ea1cbd6d65d930484740e5d67ce1c5387634a
Bogdan Andrei <bobodan41@gmail.com>
2020-04-26 11:25:58 +0300

    gui

---
+++ gui/Android.ml
@@ -0,0 +1,14 @@
+# TWRP stuff
+TW_EXCLUDE_SUPERSU := true                    # true/false: Add SuperSU or not
+TW_INCLUDE_CRYPTO := true                     # true/false: Add Data Encryption Support or not
+TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi                     # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
+DEVICE_RESOLUTION := 720x1280                 # The Resolution of your Device
+TARGET_SCREEN_HEIGHT := 1280                    # The height
+TARGET_SCREEN_WIDTH := 720                      # The width
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 80                   # Set custom brightness, low is better
+
